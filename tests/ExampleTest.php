27ec2d5e63ed604e4ed2e44c9a2391bfb3168936
Sandeep Krishnan <sandrish@sandrish.local>
2025-04-16 18:44:05 +0530

    Php 7.4 testing

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,7 +2,7 @@
 
 use Spark\Weight\Weight;
 
-it('can test clearlly passed', function () {
+it('can test clearlly passed success', function () {
     $pounds = Weight::killograms(100)->toPounds();
-    expect($pounds)->toEqual(100);
+    expect($pounds)->toEqual(220.46);
 });
